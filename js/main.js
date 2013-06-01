3e3ef46f69dfe9ce74a43329a150664824817dae
Logan <kai.ralston@gmail.com>
2013-06-01 14:29:09 -0300

    Add parse code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 /*!
- * JS code goes here
+ * Main JS code goes here
  */
 
+// Init DOM elements
 var $linkTiles = $(".link-tile");
 $linkTiles.eq(0).height($linkTiles.eq(1).height() + 32 /* Padding and Border */);
+
+// Init Parse
+Parse.initialize("cF1KaOFNgSERAxKgv4ZUDE3XBnMEpGxF2ACWmMZE", "tnNd8KSP42GsJ9ZyBVaaN9REYRW76gUj9sxm8e3i");
+
+// Init Parse objects
+var User = Parse.User;
+var Post = Parse.Object.extend("Post");
+var Comment = Parse.Object.extend("Comment");
