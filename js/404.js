6cd4136779e13800dabb430f7e8159684e28138e
katnapper323 <codypete1114@gmail.com>
2013-08-14 19:39:55 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -4,3 +4,5 @@ var winHeight = $(window).height();   // returns height of browser viewport
 var docHeight = $(document).height(); // returns height of HTML document
 var winWidth = $(window).width();   // returns width of browser viewport
 var docWidth $(document).width(); // returns width of HTML document
+
+alert("Window Dimensions. winH" + winHeight + " winW " + winWidth + "Doc Dimensions. docH" + docHeight + " docW " + docWidth)
