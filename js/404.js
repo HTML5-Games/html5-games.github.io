b6d44382bacceddc9f1018f7248c076546f27087
katnapper323 <codypete1114@gmail.com>
2013-08-14 19:33:54 -0500

    A new 404 that will use js.

---
+++ js/404.js
@@ -0,0 +1,6 @@
+//New 404 js
+
+var winHeight = $(window).height();   // returns height of browser viewport
+var docHeight = $(document).height(); // returns height of HTML document
+var winWidth = $(window).width();   // returns width of browser viewport
+var docWidth $(document).width(); // returns width of HTML document
