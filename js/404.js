e406fc53d7cd7a50b1e73119f58dad10d03077ac
katnapper323 <codypete1114@gmail.com>
2013-08-18 16:17:42 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,8 +1,23 @@
 var canvas = document.getElementById("404canvas"),
-    ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d"),
+    FPS = 30;
     
 canvas.width = 880;
 canvas.height = 600;
 
-ctx.fillStyle = "black";
-ctx.fillRect(0,0,canvas.width,canvas.height);
+
+function draw(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function update(){
+    
+}
+
+function tick(){
+    draw();
+    update();
+}
+
+setInterval(tick,1000/FPS)
