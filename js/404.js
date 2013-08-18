31b129c66e674b32b9b6480325157aa729847ab6
katnapper323 <codypete1114@gmail.com>
2013-08-18 16:22:25 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -6,9 +6,29 @@ canvas.width = 880;
 canvas.height = 600;
 
 
+function pxlBlk(x, y){
+    ctx.fillStyle = "white";
+    ctx.fillRect(x,y, 10,10);
+}
+
+
+var player = {
+    x: 440,
+    y: 500,
+    draw: function(){
+        pxlBlk(this.x + 10,this.y);
+        pxlBlk(this.x + 20,this.y);
+    },
+    update: function(){
+        
+    }
+}
+
+
 function draw(){
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
+    player.draw();
 }
 
 function update(){
