2309ac2f23516187dce3c714a57cbd4b40a9bf50
katnapper323 <codypete1114@gmail.com>
2013-08-18 16:34:46 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -15,6 +15,7 @@ function pxlBlk(x, y){
 var player = {
     x: 440,
     y: 500,
+    vx: 0,
     draw: function(){
         pxlBlk(this.x + 10,this.y);
         pxlBlk(this.x + 30,this.y);
@@ -25,7 +26,7 @@ var player = {
         pxlBlk(this.x + 40,this.y + 10);
     },
     update: function(){
-        
+        this.x += this.vx / FPS;
     }
 }
 
@@ -34,6 +35,32 @@ function draw(){
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     player.draw();
+}
+
+window.onkeydown = function( e ){
+    e = e || window.event;
+    
+    code = e.keycode || e.which;
+    
+    if(code == 37){
+        player.vx = -100;
+    }
+    if(code == 39){
+        player.vx = 100;
+    }
+}
+
+window.onkeyup = function( e ){
+    e = e || window.event;
+    
+    code = e.keycode || e.which;
+    
+    if(code == 37){
+        player.vx = 0;
+    }
+    if(code == 39){
+        player.vx = 0;
+    }
 }
 
 function update(){
