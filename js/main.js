e2c4fefb9ebe44b9dc6ac999257020d7487aa8a5
katnapper323 <codypete1114@gmail.com>
2013-10-01 22:06:09 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -268,15 +268,7 @@ function sendMessage() {
 	}
 	
 	//Emotes
-	
-	text = text.replace("<3", "\♥");
-	text = text.replace(":D", "\☻");
-	text = text.replace(":)", "\☺");
-	text = text.replace(":(", "\☹");
-	text = text.replace("(R)", "\®");
-	text = text.replace("musicnote", "\♫");
-	text = text.replace("TradeMark", "\™");
-	
+
 	// Create new message
 	var message = new Message();
 	message.set("text", text);
