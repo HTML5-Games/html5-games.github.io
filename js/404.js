e1bf828d51b417cf7201026bc6229e7a2279cf56
katnapper323 <codypete1114@gmail.com>
2013-10-06 19:01:41 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,10 +1,22 @@
 var link = location.href;
 var seconds = 30;
 
+var sec = " seconds";
+
 var tick = function(){
     seconds = seconds - 1;
+    if (seconds > 1){
+        sec = " seconds";
+    }
+    else if( seconds < 0){
+        sec = " seconds";
+        window.location.assign("http://html5-games.github.io/");
+    }
+    else {
+        sec = " second";
+    }
     document.getElementById("x").innerHTML = "Oh no, It seams that the page(" + link + ") you are looking for does not exist.";
-    document.getElementById("y").innerHTML = "You will be directed back to the home page in " + seconds + " seconds.";
+    document.getElementById("y").innerHTML = "You will be directed back to the home page in " + seconds + sec;
 }
 
 setInterval(tick,1000);
