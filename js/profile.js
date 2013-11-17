b3d79ea7ef8abaab9dcaea19a69fce29ac2dffa0
Cody Pete <codypete1114@gmail.com>
2013-11-17 15:15:21 -0600

    Yeah!

--- js/profile.js
+++ js/profile.js
@@ -5,3 +5,17 @@
 Parse.initialize("cF1KaOFNgSERAxKgv4ZUDE3XBnMEpGxF2ACWmMZE", "tnNd8KSP42GsJ9ZyBVaaN9REYRW76gUj9sxm8e3i");
 
 var Profile = Parse.Object.extend("Profile");
+
+function loadProfile(){
+  var query = new Parse.Query(Profile);
+  var currentUser = Parse.User.current();
+  query.equalTo("user", currentUser);
+  query.first({
+    success: function(object){
+      document.getElementById("aboutProfile").innerHTML = object.get("aboutMe");
+    },
+    error: function(error){
+      alert("Error receiving Profile. Code:" error.code + " " + error.message);
+    }
+  });
+}
