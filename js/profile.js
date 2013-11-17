ed93068b19a62b2c5306ffa49c5a6b5140b92d07
Cody Pete <codypete1114@gmail.com>
2013-11-17 15:02:34 -0600

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,7 @@
+//Profile Javascript
+
+
+//Not sure if I need to initialize again, but better safe than sorry.
+Parse.initialize("cF1KaOFNgSERAxKgv4ZUDE3XBnMEpGxF2ACWmMZE", "tnNd8KSP42GsJ9ZyBVaaN9REYRW76gUj9sxm8e3i");
+
+var Profile = Parse.Object.extend("Profile");
