c4e1299d0b74cbdb3a65ddfcb83efe13236f4591
Cody Pete <codypete1114@gmail.com>
2013-11-18 18:29:06 -0600

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -48,5 +48,5 @@ function saveProfile(){
 function loadColor(){
         var color = "#" + document.getElementById("red").innerHTML  + document.getElementById("green").innerHTML + document.getElementById("blue").innerHTML;
         
-        document.getElementById("color").style.background-color = String(color);
+        document.getElementById("color").style.background = String(color);
 }
